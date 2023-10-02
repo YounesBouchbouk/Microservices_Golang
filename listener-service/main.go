8fb4d8688b7b21b1b680e37dbd17152a237db62a
younes bouchbouk <younesbouchbouk.py@gmail.com>
2023-10-02 11:38:10 +0100

    add listener microservice with rabbitMq

---
+++ listener-service/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+
+	//try to connect to rabbitMQ
+	rabbitConn, err := connect()
+
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	defer rabbitConn.Close()
+	fmt.Println("connected to rabbitMq !")
+
+	//start listening for messages
+
+	// watch the queue and consume events
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+
+	var connection *amqp.Connection
+
+	for {
+		c, err := amqp.Dial("amqp:guest:guest@localhost")
+
+		if err != nil {
+			fmt.Println("Rabbit mq not ready to be connected yet ")
+			counts++
+		} else {
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+
+		fmt.Println("backing off ...")
+		time.Sleep(backOff)
+		continue
+	}
+
+	return connection, nil
+
+}
